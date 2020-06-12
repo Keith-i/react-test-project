3bb8248448f2f46ab40338717ee4176a6eb97d0e
Keith <754781293@qq.com>
2020-06-12 17:04:15 +0800

    提交

---
+++ server/routes/users.js
@@ -0,0 +1,12 @@
+const { request } = require("express");
+
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.send({
+    msg: 'hello!'
+  })
+})
+
+module.exports = router
