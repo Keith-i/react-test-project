3c5f0367cad98b7401391f92536cb607f61c2372
Keith <754781293@qq.com>
2020-06-15 18:11:45 +0800

    提交

--- server/index.js
+++ server/index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const users = require("./routes/users")
 const debug = require("debug")("my-application")
+const bodyParser = require("body-parser")
 
+
+app.use(bodyParser.json())
 app.use('/api/users', users)
 
 app.listen(3030, (req, res) => {
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,43 @@
-const { request } = require("express");
+// const { request } = require("express");
 
 const express = require('express')
 const router = express.Router()
+const isEmpty = require('lodash/isEmpty')
+const validator = require('validator')
 
-router.get('/', (req, res) => {
-  res.send({
-    msg: 'hello!'
-  })
+// const isEmpty = require('lodash/isEmpty')
+// const validator = require("validator")
+
+const validatorInput = (data) => {
+  let errors = {}
+  if (validator.isEmpty(data.username)) {
+    errors.username = "请填写用户名"
+  }
+  if (!validator.isEmail(data.email)) {
+    errors.email = "请填写邮箱"
+  }
+  if (validator.isEmpty(data.password)) {
+    errors.password = "请填写密码"
+  }
+  if (validator.isEmpty(data.passwordConfirmation)) {
+    errors.password = "请确认密码"
+  }
+  if (!validator.equals(data.password, data.passwordConfirmation)) {
+    errors.passwordConfirmation = "两次密码不相同"
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  }
+}
+
+router.post('/', (req, res) => {
+  console.log(req.body, 'aaa')
+  const {errors, isValid} = validatorInput(req.body)
+  if (!isValid) {
+    res.status(400).json(errors)
+  }
 })
 
 module.exports = router
